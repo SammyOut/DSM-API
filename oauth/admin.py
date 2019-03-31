f6093e322e5cef1c65b4a5245119d84e8cee3d89
Sangmin In <python@istruly.sexy>
2019-03-31 11:19:51 +0900

    django admin

--- oauth/admin.py
+++ oauth/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from oauth import models
 
-# Register your models here.
+
+class StudentModelAdmin(admin.ModelAdmin):
+    list_display = ('number', 'name', 'username', 'email')
+
+
+class AppModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'owner')
+
+
+class RefreshTokenAdmin(admin.ModelAdmin):
+    list_display = ('app', 'student')
+
+
+class AccessTokenModelAdmin(admin.ModelAdmin):
+    list_display = ('app', 'student')
+
+
+class ServiceModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+class AppServiceRelModel(admin.ModelAdmin):
+    list_display = ('app', 'service')
+
+
+admin.site.register(models.StudentModel, StudentModelAdmin)
+admin.site.register(models.AppModel, AppModelAdmin)
+admin.site.register(models.RefreshTokenModel, RefreshTokenAdmin)
+admin.site.register(models.AccessTokenModel, AccessTokenModelAdmin)
+admin.site.register(models.ServiceModel, ServiceModelAdmin)
+admin.site.register(models.AppServiceRelModel, AppServiceRelModel)
