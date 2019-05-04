308eb72af9690e606c4f1cff2cd2a3852db30173
Sangmin In <python@istruly.sexy>
2019-05-05 00:29:20 +0900

    move template, static directory to base directory

--- dsm_api/settings.py
+++ dsm_api/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dsm_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,5 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'oauth/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
